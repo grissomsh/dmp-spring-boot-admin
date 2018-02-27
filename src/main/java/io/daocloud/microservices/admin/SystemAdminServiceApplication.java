1cd8e83b0a982d0ecaa2c362b3bfadc2447e9f64
Grissom <grissom.wang@daocloud.io>
2018-02-27 17:20:11 +0800

    update

--- src/main/java/io/daocloud/microservices/admin/SystemAdminServiceApplication.java
+++ src/main/java/io/daocloud/microservices/admin/SystemAdminServiceApplication.java
@@ -1,12 +1,19 @@
 package io.daocloud.microservices.admin;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+import com.spring4all.swagger.EnableSwagger2Doc;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
 @SpringBootApplication
 @EnableAdminServer
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableSwagger2Doc
 public class SystemAdminServiceApplication {
 
 	public static void main(String[] args) {
